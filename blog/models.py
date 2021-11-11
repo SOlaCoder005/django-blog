3e6923fcd4c743f59a46f39ab8d2722ac38a6bb8
SOlaCoder005 <misssamfelicia@gmail.com>
2021-11-11 19:15:54 +0000

    final commit

--- blog/models.py
+++ blog/models.py
@@ -1,28 +1,25 @@
 from django.db import models
-from django.contrib.auth.models import User 
+from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
-# Create your models here.
 
 
-STATUS = ((0, "Draft"), (1, " Published"))
+STATUS = ((0, "Draft"), (1, "Published"))
 
 
 class Post(models.Model):
-    """
-        - This class should correlate with the key features of the post object
-        - Such features can be drafted in a table prior to creating the
-        model
-    """
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
-    updated_on = models.DateTimeField(auto_now=True)
-    content = models.TextField(default="placeholder")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
     featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    likes = models.ManyToManyField(User, related_name="blog_likes", blank=True)
+    likes = models.ManyToManyField(
+        User, related_name='blogpost_like', blank=True)
 
     class Meta:
         ordering = ["-created_on"]
@@ -34,12 +31,11 @@ class Post(models.Model):
         return self.likes.count()
 
 
-
 class Comment(models.Model):
-
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
-    name = models.CharField(max_length=200, unique=True)
-    email_address = models.EmailField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
@@ -49,4 +45,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment {self.body} by {self.name}"
-
